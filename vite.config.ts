cd9c5de3ab8c2e2c853e2e1b162b72408e8dd726
wallace fares <wallacefares@gmail.com>
2022-11-13 12:18:57 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import slugify from 'slugify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '.',
+  base: './',
   plugins: [svelte()],
   optimizeDeps: {
     disabled: false,
